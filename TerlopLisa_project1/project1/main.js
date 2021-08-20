9bab88720190ae247fa26b954487743790e6ff2a
TerlopLisa-FS <LMTerlop@student.fullsail.edu>
2021-08-19 22:05:38 -0400

    added more pseudocode to explain the application and ran through application one more time checking for errors

--- TerlopLisa_project1/project1/main.js
+++ TerlopLisa_project1/project1/main.js
@@ -7,17 +7,21 @@ let accountCheck = prompt("Do you already have an account? Yes/No");
 //Check to make sure the user only enters "yes" or "no"
 while(accountCheck != "yes" && accountCheck != "no")
 {
+    //Display error message & prompt user to say if they have an account or not
     accountCheck = prompt("Please type yes or no only. Do you already have an account? Y/N");
 }
 //need conditional for if the user has an account then SignIn will run, if not SignUp will run
 if(accountCheck == "yes")
 {
+    //Call the sign in function since the user typed in yes
     SignIn(usernames,passwords);
 }
 else
 {
+    //Variables for the parameters of the SignUp function
     let user;
     let password;
+    //Call the SignUp function since the user answered no
     SignUp(user,password);
 }
 
@@ -29,13 +33,15 @@ function SignIn(username,password)
     //Loop through the usernames and passwords in the arrays
     for(var i=0; i<usernames.Length; i++)
     {
-
+        //checking if the username and password matched elements in array
         if((username === usernames[i]) &&(password === passwords[i]))
         {
+            //feedback message for user
             console.log("You have successfully logged in");
         }
         else
         {
+            //Error message
             console.log("Please try logging in again");
         }
     }
@@ -48,6 +54,7 @@ function SignUp()
     //Validate not left blank
     while(getUsername === "")
     {
+        //Display error message and reprompt user
         getUsername = prompt("Please enter a username and do not leave blank. Username: ");
     }
     //Have user type in a password
@@ -57,5 +64,6 @@ function SignUp()
     {
         getPassword = prompt("Please enter a password and do not leave blank. Password: ");
     }
+    //Feedback message for user
     console.log("Username and password created!");
 }

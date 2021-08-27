7f25ff5bd1d8366a0689c9bc0052f406f951aa8d
TerlopLisa-FS <LMTerlop@student.fullsail.edu>
2021-08-27 19:38:39 -0400

    completed final project

--- TerlopLisa_project1/project1/main.js
+++ TerlopLisa_project1/project1/main.js
@@ -1,38 +1,31 @@
 //username and password arrays
-var usersArray = {
-    "lterlop1" : "passcode1",
-    "jessG1" : "passcode2",
-    "tjC1" : "passcode3",
-    "danS1": "passcode4"};
-
+var usersArray = ["lterlop1","jessG1","tjC1","danS1"];
+var heading2 = document.getElementById("heading2");
+var password = "javaScriptApp";
 var signInButton = document.getElementById("signInButton").onclick = SignIn;
 function SignIn()
 {
     //Ask user for their username and password
      var user = prompt("Username: ");
      var pass = prompt("Password: ");
-     while(user == "" || pass == "")
-     {
-         alert("Please do not leave blank!");
-         user = prompt("Username: ");
-         pass = prompt("Password: ");
-     }
 
     //Loop through the usernames and passwords in the arrays
     for(var i = 0; i < usersArray.length; i++)
     {
         //checking if the username and password matched elements in array
-        if (user && pass === usersArray[i])
+        if (user === usersArray[i] && pass === password)
         {
             //feedback message for user
-            document.getElementById("heading1").innerText = "Welcome back to myCollectible";
-            document.getElementById("heading1").style.color = "blue";;
+            heading2.innerText = "Welcome back to myCollectible!";
+            heading2.style.color = "blue";
+            break;
         }
         else
         {
             //Error message
-            document.getElementById("heading2").innerText = "Please try logging in again";
-            document.getElementById("heading2").style.color = "red";
+            heading2.innerText = "Please try logging in again.";
+            heading2.style.color = "red";
+
         }
 
     }
@@ -56,7 +49,8 @@ function SignUp()
     {
         getPassword = prompt("Please enter a password and do not leave blank. Password: ");
     }
+
     //Feedback message for user
-    document.getElementById("heading2").innerText = "Sign up successful! Welcome to myCollectible";
-    document.getElementById("heading2").style.color = "blue";
+    heading2.innerText = "Sign up successful! Welcome to myCollectible";
+    heading2.style.color = "blue";
 }
